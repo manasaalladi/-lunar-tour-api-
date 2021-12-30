15c17d18fae6ac48c5fdc23385d457d83cd959dc
alladimanasa <manasa.alladi@yottatechports.com>
2021-12-29 23:36:47 -0500

    Commit

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -8,10 +8,12 @@ export const getAllListings = async (args, context) => {
     TableName: process.env.ListingsDB || "dev-lunar-listings",
   };
 
-  
+  //try {
+  //  const result = await dynamodb.scan(params);
+
     try {
-      const result = await dynamoDBLib.call("scan", params)
-    
+  const result = await dynamoDBLib.call("scan", params)
+
     if (result.Items.length === 0) {
       return "You have no listings";
     } else {
